db4e402e29fb13eb2df1ffddd4c7136694d6bb25
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2021-11-07 10:25:39 +0100

    BmiMapper and BmiFacade classes added

--- src/main/java/business/persistence/BmiMapper.java
+++ src/main/java/business/persistence/BmiMapper.java
@@ -1,5 +1,18 @@
 package business.persistence;
 
+import business.exceptions.UserException;
+
 public class BmiMapper
 {
+    private Database database;
+
+    public BmiMapper(Database database)
+    {
+        this.database = database;
+    }
+
+    public void insertBmiEntry() throws UserException
+    {
+        // TODO: insert data into database.
+    }
 }
